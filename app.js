cf5e50c7ad2b07a4917bbd65f3ea22027488f51d
Alberto Seidi <seiji_alberto@hotmail.com>
2022-05-30 12:00:02 -0300

    salvando arquivos no FS

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const { randomUUID } = require("crypto");
+const fs = require("fs");
+const { json } = require("express/lib/response");
+
 const app = express();
 app.use(express.json());
-const products = [];
+let products = [];
+
+fs.readFile("products.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    products = JSON.parse(data);
+  }
+});
 
 /* 
 POST => Inserir um dado
@@ -25,11 +36,45 @@ app.post("/products", (req, res) => {
 
   products.push(product);
 
+  fs.writeFile("products.json", JSON.stringify(products), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Produto inserido");
+    }
+  });
   return res.json(product);
 });
 
 app.get("/products", (req, res) => {
   return res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = products.find((product) => product.id === id);
+  return res.json(product);
+});
+
+app.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  const productIndex = products.findIndex((product) => product.id === id);
+  products[productIndex] = {
+    ...products[productIndex],
+    name,
+    price,
+  };
+  return res.json({ message: "Produto Alterado com sucesso" });
+});
+
+app.delete("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const productIndex = products.findIndex((product) => product.id === id);
+  products.splice(productIndex, 1);
+
+  return res.json({ message: "Produto removido com sucesso" });
 });
 
 app.listen(4002, () => console.log("Servidor esta rodando na porta 4002..."));
